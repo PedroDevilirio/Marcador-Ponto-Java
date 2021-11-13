a9e7b7382118b427816925e8c04a4e4108a9b3a6
Pedro Devilirio <036pedro.cardoso@gmail.com>
2021-11-13 09:50:43 -0300

    A estrutura if foi alterada e alocada dentro de um método, a função main foi fechada.

--- Documents/Petfinder/Aula1/src/Ponto.java
+++ Documents/Petfinder/Aula1/src/Ponto.java
@@ -4,35 +4,34 @@ import java.util.Date;
 
 
 class Ponto {
-	public static void main (String[] args) {
-		Scanner marcador = new Scanner (System.in);
-		Calendar registro = Calendar.getInstance();
+	public static void main (String[] args){
+		public Scanner marcador = new Scanner (System.in);
+		public Calendar registro = Calendar.getInstance();
 		
-		// Variavel em que o horário fixo de entrada será definido.
-		Date horario = 11:00:00;
-		
-		// Variavel em que o horario que o funcionario chegou será marcado
-		Date horarioEntrada;
-		
-		// Variavel que checa a decisão do usuário
-		int simNao;
-		
-		System.out.println("Bem-vindo a FakeTech E.P. Me diga, você é um: \n[1] Funcionário \n[2] Cliente");
-		simNao = marcador.nextInt();
-		
-		if (simNao == 1) {
-			System.out.println("Deseja marcar seu ponto de entrada agora? :D \n[1] Sim \n[2] Não");
-		simNao = marcador.nextInt();
-		
-		if (simNao == 1) {
-			horarioEntrada = registro.getTime();
-			System.out.println("Horário marcado: "+ horarioEntrada + ".");
-		}else {
-			System.out.println("Aplicação encerrada!");
-		}
+		public Date horarioEntrada;
+	
+		public void setPoint(int choice) {
 			
-		}else {
-			System.out.println("Bem-vindo, cliente! Sinto informar que essa área é restrita para funcionários. :(");
+			System.out.println("Bem-vindo a FakeTech E.P. Me diga, você é um: \n[1] Funcionário \n[2] Cliente");
+			
+			choice = marcador.nextInt();
+			
+			if (choice == 1) {
+				System.out.println("Deseja marcar seu ponto de entrada agora? :D \n[1] Sim \n[2] Não");
+				int secondChoice = marcador.nextInt();
+				if (secondChoice == 1) {
+					horarioEntrada = registro.getTime();
+					System.out.println("Horário registrado. " + horarioEntrada);
+				}else {
+					System.out.println("Tudo bem, foi um prazer te atender!.");
+					return;
+				}
+			}else if(choice == 2){
+				System.out.println("Esta função é exclusiva para funcionários.");
+				return;
+			}else {
+				System.out.println("Desculpa, não reconheço este comando!");
+				return;
+			}
 		}
 	}
-}
